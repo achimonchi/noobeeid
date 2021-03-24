370d41766cc62f07d0dfe78638b224c576e5bc13
Reyhan Jovie <reyhanjovie01@gmail.com>
2021-03-24 20:47:37 +0700

    add animation on banner

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import {useRef, useEffect} from "react";
-import {TweenMax} from "gsap";
+import {TweenMax, TimelineLite, Power3} from "gsap";
 
 export default function Home() {
   return (
@@ -19,15 +19,27 @@ const Banner=()=>{
   let banner = useRef(null);
   let images = useRef(null);
 
+  const tl = new TimelineLite();
+
   useEffect(()=>{
-    TweenMax.to(banner, 0, {css:{visibility: 'visible'}})
+    // TweenMax.to(banner, 0, {css:{visibility: 'visible'}})
+    const headline = banner.children[0];
+    const subHeadline = banner.children[1];
+    console.log({headline, subHeadline});
+
+    tl.from(images, 1.2, {scale:0.5, opacity:0, ease: Power3.easeInOut}, .2)
+    // tl.from(banner, 1, {x:-500, opacity:0, ease: Power3.easeInOut}, .5)
+
+    // content animation
+    // tl.s
+    tl.staggerFrom(banner.children, 1, {y:44, opacity:0, ease: Power3.easeInOut, delay: 0.8}, .3);
   }, [])
 
   return(
-    <section id="banner" className="banner h-full pt-10 md:pt-20 invisible" ref={el=>banner = el} >
+    <section id="banner" className="banner h-full pt-10 md:pt-20" >
       <div className="container mx-auto h-full">
         <div className="grid grid-cols-12 h-full">
-          <div className="lg:col-span-4 col-span-12 flex flex-col justify-start lg:justify-center top-0 h-full lg:p-5 px-5 order-2 lg:order-1 ">
+          <div ref={el=>banner = el} className="lg:col-span-4 col-span-12 flex flex-col justify-start lg:justify-center top-0 h-full lg:p-5 px-5 order-2 lg:order-1 ">
             <h1 className="w-full md:w-10/12 mx-auto lg:w-full text-2xl md:text-4xl lg:text-5xl text-center lg:text-left">Sudah siap untuk menuju era <span className="font-bold">Go-Digital</span> ?</h1>
               <h2 className="text-xs md:text-xl lg:text-base mt-2 lg:mt-5 text-center md:w-10/12 lg:w-full md:mx-auto lg:text-left "><b>NooBeeID</b> adalah <b>Full Services Digital Agensi</b> yang siap membantu kamu untuk <b>go-digital</b>.</h2>
             <div className="grid grid-cols-12">
